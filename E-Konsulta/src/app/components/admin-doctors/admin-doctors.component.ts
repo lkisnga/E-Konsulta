7446596cfe1c94e7ac1ff67c081895a6d0669d8f
JanLance <janelmerlance.igot@ctu.edu.ph>
2021-10-08 09:36:19 +0800

    added Doctor List

--- E-Konsulta/src/app/components/admin-doctors/admin-doctors.component.ts
+++ E-Konsulta/src/app/components/admin-doctors/admin-doctors.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { element } from 'protractor';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin-doctors',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDoctorsComponent implements OnInit {
 
-  constructor() { }
+  userID: string = "";
+  list: any = [];
+  list2: any = [];
+  constructor(public authservice : AuthService, public userservice : UserService, public db : AngularFirestore
+    ,public router : Router) { }
 
   ngOnInit(): void {
+    this.userID = this.authservice.get_UID();
+    var data;
+    var tempArray = [];
+    this.userservice.get_doctorList().then(res=>{
+      res.forEach(function(doc){
+        data =  doc.data();
+        data.uid = doc.id;
+        tempArray.push(data);
+      })
+      this.list = tempArray;
+    })
+  }
+
+  editInfo(record)
+  {
+    console.log(record.uid);
   }
 
 }
