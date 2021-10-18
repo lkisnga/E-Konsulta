ed9b2fc317b8e96a53bff4b328e1bae82716f69e
JanLance <janelmerlance.igot@ctu.edu.ph>
2021-10-18 15:24:51 +0800

    updates

--- E-Konsulta/src/app/components/lab-partner-result/lab-partner-result.component.ts
+++ E-Konsulta/src/app/components/lab-partner-result/lab-partner-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-lab-partner-result',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lab-partner-result.component.css']
 })
 export class LabPartnerResultComponent implements OnInit {
-
-  constructor() { }
+  file : any;
+  labId : string="";
+  constructor(public userservice : UserService, public afu : AuthService) { }
 
   ngOnInit(): void {
+    this.labId = this.afu.get_UID();
+  }
+  choosefile(e)
+  {
+    this.file = e.target.files[0];
+    console.log(this.file);
+  }
+  //Passing file to the user
+  uploadfile()
+  {
+    this.userservice.lab_fileUpload(this.file,this.labId,"testingID");
   }
 
 }
