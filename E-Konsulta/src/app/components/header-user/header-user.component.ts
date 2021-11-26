38c93326e8b27328705f9ae38fdf3ee337215eaa
JanLance <janelmerlance.igot@ctu.edu.ph>
2021-11-26 17:53:57 +0800

    update: added admin notification

--- E-Konsulta/src/app/components/header-user/header-user.component.ts
+++ E-Konsulta/src/app/components/header-user/header-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-header-user',
@@ -7,17 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderUserComponent implements OnInit {
 
-  constructor() {
+  userId : string = "";
+  notifList : any = [];
+  badge : string = sessionStorage.getItem('Notification');
+  nof_flag: boolean = false; //notif sound
+  flag : boolean = false; 
+  constructor(
+    public afu : AuthService,
+    public notif : NotificationService
+  ) {
   }
   notification = false;
   notifFunction(){
     this.notification = true;
+
+    sessionStorage.setItem('Notification','false');
+    this.badge = sessionStorage.getItem('Notification');
   }
 
   doubleClick(e: Event) {
     this.notification = false;
   }
   ngOnInit(): void {
+
+    if(sessionStorage.getItem('Notification')==null)
+      sessionStorage.setItem('Notification','false')
+
+    this.userId = this.afu.get_UID();
+    this.notif.notif_soundAdmin(this.userId,this.nof_flag);
+    this.get_notification()
+  }
+  get_notification()
+  {
+    var data;
+    var tempArray = [];
+    this.notif.get_admin(this.userId).onSnapshot(snapshot=>{
+      let changes = snapshot.docChanges();
+      changes.forEach(e=>{
+        data = e.doc.data();
+        if(e.type == 'added' && this.flag == false)
+        {
+          tempArray.push(data);
+        }
+        if(e.type == 'added' && this.flag == true)
+        {
+          sessionStorage.setItem('Notification','true');
+          this.badge = sessionStorage.getItem('Notification');
+          console.log(this.badge);
+          tempArray.unshift(data);
+        }
+      })
+      this.flag = true;
+    })
+    this.notifList = tempArray;
   }
 
 }
