f913327e858c7100d8d1b26fd12ce4f6b2e11cbc
JanLance <janelmerlance.igot@ctu.edu.ph>
2021-11-29 12:54:11 +0800

    update: insurance

--- E-Konsulta/src/app/components/health-insurance-verification-patient/health-insurance-verification-patient.component.ts
+++ E-Konsulta/src/app/components/health-insurance-verification-patient/health-insurance-verification-patient.component.ts
@@ -1,4 +1,7 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-health-insurance-verification-patient',
@@ -6,22 +9,111 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./health-insurance-verification-patient.component.css']
 })
 export class HealthInsuranceVerificationPatientComponent implements OnInit {
+  userId : string="";
+  list: any = [];
+  list2: any = [];  //for verified
 
-  pending1 = true;
-  verified1 = false;
-  constructor() { }
+  pending : boolean = true;
+  verified : boolean = false;
+
+  userIns : any = [];
+  balance : number= 0;
+
+  patient_id : string = "";
+  constructor(public afu : AuthService, public userservice : UserService) { }
+
+  isVerified_Doc(e)
+  {
+    if(e=='pending')
+      {
+        this.verified = false;
+        this.pending = true;
+      }
+    if(e=='verified')
+    {
+      this.pending = false;
+      this.verified = true;
+    }
+  }
 
   ngOnInit(): void {
+    this.userId=this.afu.get_UID();
+
+    this.patientList();
   }
 
-  pending(){
-    this.pending1 = true;
-    this.verified1 = false;
+  patientList()
+  {
+    var data;
+    var tempArray = [],tempArray2 = [];
+    this.userservice.get_insurance_verification(this.userId).then(e=>{
+      e.forEach(item=>{
+        if(item.data().isVerified == 'pending')
+        {
+        data = item.data();
+        data.uid = item.id;
+        tempArray.push(data);
+        }
+        if(item.data().isVerified == 'verified')
+        {
+          data = item.data();
+          data.uid = item.id;
+          tempArray2.push(data);
+        }
+      })
+    })
+    this.list2 = tempArray2;
+    this.list = tempArray;
   }
 
-  verified(){
-    this.pending1 = false;
-    this.verified1 = true;
+  get_patient_insuranceInfo()
+  {
+
+  }
+
+  verify(e,stats)
+  {
+    this.userservice.verify_userInsurance(e.uid,stats).then(()=>{
+      console.log("Successfully Verified!");
+      this.ngOnInit();
+    })
+  }
+  edit_info(info)
+  {
+    this.patient_id = info.uid;
+    this.userIns = [];
+    var data;
+    this.userservice.get_patient_insuranceInfo(info.uid,this.userId).then(e=>{
+      e.forEach(item=>{
+        data = item.data();
+        data.uid = item.id;
+        data.updatedAt = item.data().updatedAt;
+        this.userIns = data;
+      })
+    }).then(()=>{
+      this.balance = this.userIns.limit - this.userIns.spent;
+    })
+  }
+  update_info(info)
+  {
+    let record = {};
+    record = info;
+    record['health_insurance'] = this.userId;
+    record['updatedAt'] = formatDate(new Date(),'MM/dd/yyyy','en');
+    if(this.userIns.uid == undefined)
+    {
+      this.userservice.create_patient_insuranceInfo(this.patient_id,record)
+      .then(()=>{
+        console.log("Created!");
+      })
+    }
+    else
+    {
+      this.userservice.update_patient_insuranceInfo(this.patient_id,this.userIns.uid,record)
+      .then(()=>{
+        console.log('Updated!');
+      })
+    }
   }
 
 }
