65f45b26bbd274659149b82dcdd3159515a6cf96
JanLance <janelmerlance.igot@ctu.edu.ph>
2021-12-12 16:39:50 +0800

    updates

--- E-Konsulta/src/app/components/doctor-user-analytics/doctor-user-analytics.component.ts
+++ E-Konsulta/src/app/components/doctor-user-analytics/doctor-user-analytics.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
@@ -13,6 +14,8 @@ export class DoctorUserAnalyticsComponent implements OnInit {
 
   total: number = 0;
   today: number = 0;
+  currentEarning: number = 0;
+  totalEarning: number = 0;
 
   constructor(
     public userservice : UserService,
@@ -23,6 +26,7 @@ export class DoctorUserAnalyticsComponent implements OnInit {
     this.userId = this.afu.get_UID();
     this.get_consultations();
     this.get_today();
+    this.get_earnings();
   }
 
   get_consultations()
@@ -35,6 +39,20 @@ export class DoctorUserAnalyticsComponent implements OnInit {
   {
     this.userservice.get_today_consultation(this.userId).then(e=>{
       this.today = e.size;
+    })
+  }
+  get_earnings(){
+    this.userservice.get_doctorEarning(this.userId)
+    .then(e=>{
+      e.forEach(item=>{
+        var date = (new Date(item.data().createdAt).getMonth()+1)+'/'+new Date(item.data().createdAt).getDate()+'/'+new Date(item.data().createdAt).getFullYear();
+        if(date.match(formatDate(new Date(),'M/d/yyyy','en')))
+        {
+          console.log('true');
+          this.currentEarning+=parseFloat(item.data().net_income);
+        }
+        this.totalEarning += item.data().net_income;
+      })
     })
   }
 
