18e8b74fc5cfb130ef2ec0e98be043cb68ec3856
JanLance <janelmerlance.igot@ctu.edu.ph>
2022-02-26 13:36:17 +0800

    added template for medical certificate

--- E-Konsulta/src/app/components/medical-certificate/medical-certificate.component.ts
+++ E-Konsulta/src/app/components/medical-certificate/medical-certificate.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-medical-certificate',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medical-certificate.component.css']
 })
 export class MedicalCertificateComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('content') content: ElementRef;
+  patientInfo: any = [];
+  doctorInfo: any = [];
+  user_id: string = "";
+  datetoday: string = "";
+  monthDay: string = "";
+  constructor(
+    public afu: AuthService,
+    public userservice: UserService
+  ) { }
 
   ngOnInit(): void {
+    
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+     
+    this.user_id = this.afu.get_UID();
+    this.patientInfo = JSON.parse(localStorage.getItem('mc'));
+    this.get_doctorInfo();
+    this.datetoday = formatDate(new Date(),'long','en');
+
+    const d = new Date();
+    let name = month[d.getMonth()];
+    this.monthDay = name + ' ' + new Date().getDate();
+
+    if(localStorage.getItem('mc')!=null)
+    {
+      setTimeout(() => {
+        this.downloadPDF();
+        localStorage.removeItem('mc');
+      }, 1800);
+    }
   }
 
+  get_doctorInfo()
+  {
+    var data;
+    this.userservice.get_UserInfo(this.user_id)
+    .then(e=>{
+      this.userservice.get_specializationInfo(e.data().specialization)
+      .then(res=>{
+        data = e.data();
+        data.spc_name = res.data().name;
+        this.doctorInfo = data;
+        console.log(this.doctorInfo)
+      })
+    })
+  }
+  public downloadPDF()
+  {
+    html2canvas(document.body).then(canvas=>{
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('l','mm','a4');
+      var width = pdf.internal.pageSize.getWidth();
+      var height = canvas.height * width / canvas.width;
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, width, height);
+      pdf.save('medicalCertificate.pdf'); 
+    })
+  }
 }
